9cb077799552192b914d30702885b3d2a8ae0308
Ethicdev07 <akinadejunior76@gmail.com>
2026-08-20 10:46:34 +0100

    database connection setup

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = require('./app');
 
 const port = process.env.PORT || 5000;
 
+const connectDB = require('./src/config/db');
+
+connectDB();
+
 app.listen(port, ()=>{
     console.log(`Server is runing on port ${port}`);
     
