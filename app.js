e62afd7fcf257745eec36fe88ca617d6e750431b
Ethicdev07 <akinadejunior76@gmail.com>
2026-09-23 10:30:20 +0100

    code base updated

--- app.js
+++ app.js
@@ -12,7 +12,19 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
-app.use(cors({ origin: process.env.FRONTEND_URL }));
+
+const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:5173"]
+    .filter(Boolean)
+    .map((url) => url.replace(/\/+$/, ""));
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin.replace(/\/+$/, ""))) {
+            return callback(null, true);
+        }
+        return callback(new Error("Not allowed by CORS"));
+    },
+}));
 app.use(morgan("dev"))
 
 
